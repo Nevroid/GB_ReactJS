4368bd78f7667a2c8e58acaf787f82a7a3651ed3
KirTim <kirtim1986@gmail.com>
2021-08-13 17:29:16 +0300

    hw_09_in_progress

--- gb_react_proj/src/store/actions/profileSetData.js
+++ gb_react_proj/src/store/actions/profileSetData.js
@@ -1,9 +1,17 @@
 export const PROFILE_SET_DATA = 'PROFILE_SET_DATA'
+export const PROFILE_SET_ISAUTHED = 'PROFILE_SET_ISAUTHED'
 
 export const profileSetData = (name, age) => ({
     type: PROFILE_SET_DATA,
     payload: {
         name,
         age
+    }
+})
+
+export const profileSetIsAuthed = (isAuthed) => ({
+    type: PROFILE_SET_ISAUTHED,
+    payload: {
+        isAuthed
     }
 })
